d7d9b10a082efeb968472957663ca9457401ac0d
candy0530 <michelle780530@hotmail.com>
2017-11-06 22:44:57 +0800

    Add example to use API which provided from Microsoft Azure to detect text.

---
+++ microsoft_detect_text.py
@@ -0,0 +1,37 @@
+import httplib, urllib, base64, json
+
+subscription_key = '47cb7f2b0b2d4eb6bf268b50f3fca6d9'
+
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.urlencode({
+    # Request parameters. The language setting "unk" means automatically detect the language.
+    'language': 'unk',
+    'detectOrientation ': 'true',
+})
+
+# The URL of a JPEG image containing text.
+body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png'}"
+
+try:
+    # Execute the REST API call and get the response.
+    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print('Error:')
+    print(e)
